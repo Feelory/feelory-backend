f8582acbfc07dca1006a07f6f2c94cab58833403
DEV_KANGSAN <devstar1223@gmail.com>
2025-07-10 16:44:11 +0900

    🎉 Initial commit

---
+++ src/main/java/com/feelory/feelory_backend/FeeloryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.feelory.feelory_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeloryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeeloryBackendApplication.class, args);
+	}
+
+}
