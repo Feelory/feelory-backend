9f11436c425c18ae85492ec794b5b40023e0d756
TeakRyeol Yoo <80720210+tryoo0607@users.noreply.github.com>
2025-07-16 23:32:56 +0900

    Update src/main/java/com/feelory/feelory_backend/words/controller/CategoriesController.java
    Co-authored-by: 간산 <167552331+devstar1223@users.noreply.github.com>

--- src/main/java/com/feelory/feelory_backend/words/controller/CategoriesController.java
+++ src/main/java/com/feelory/feelory_backend/words/controller/CategoriesController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CategoriesController {
 
-    CategoriesService categoriesService;
+    private final CategoriesService categoriesService;
 
     @GetMapping("")
     public CategoriesResponse getCategories(CategoriesRequest request) {
