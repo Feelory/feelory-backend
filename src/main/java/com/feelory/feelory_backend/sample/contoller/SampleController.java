8af896a67d5e1129f9df7aedb75f78b4443e6b20
DEV_KANGSAN <devstar1223@gmail.com>
2025-07-16 17:06:41 +0900

    ✨ feat(Sample): 테스트용 DB 데이터 저장 API 추가

--- src/main/java/com/feelory/feelory_backend/sample/contoller/SampleController.java
+++ src/main/java/com/feelory/feelory_backend/sample/contoller/SampleController.java
@@ -1,23 +1,22 @@
 package com.feelory.feelory_backend.sample.contoller;
 
 import com.feelory.feelory_backend.sample.service.SampleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/sample")
 public class SampleController {
 
+    private final SampleService sampleService;
+
     @Value("${sample.hello}")
     private String STR_HELLO;
-
-    SampleService sampleService;
-
-    public SampleController(SampleService sampleService) {
-        this.sampleService = sampleService;
-    }
 
     @GetMapping("hello")
     public String sendHelloWorld() {
@@ -29,5 +28,12 @@ public class SampleController {
     public String sendSampleTextFromDB() {
 
         return sampleService.getSampleTextFromDB();
+    }
+
+    @PostMapping("db-data")
+    public String insertSampleText() {
+        sampleService.insertSampleText();
+
+        return "DB 데이터 저장 완료";
     }
 }
