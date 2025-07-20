05b45f8d28e766e38177e5352a3bed1d4eb2ddd9
DEV_KANGSAN <devstar1223@gmail.com>
2025-07-20 23:50:35 +0900

    ✨ feat(Sample): 예외 처리 테스트를 위한 메서드 추가

--- src/main/java/com/feelory/feelory_backend/sample/contoller/SampleController.java
+++ src/main/java/com/feelory/feelory_backend/sample/contoller/SampleController.java
@@ -36,4 +36,16 @@ public class SampleController {
 
         return "DB 데이터 저장 완료";
     }
+
+    @GetMapping("throw-base-exception")
+    public String throwBaseException() {
+        sampleService.throwBaseException();
+        return "이 메시지는 절대 도달하지 않습니다.";
+    }
+
+    @GetMapping("throw-unknown-exception")
+    public String throwUnknownException() {
+        sampleService.throwUnknownException();
+        return "이 메시지도 도달하지 않습니다.";
+    }
 }
