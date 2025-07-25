2123640da20c38c4f976febce905a17c3614907a
DEV_KANGSAN <devstar1223@gmail.com>
2025-07-25 13:45:12 +0900

    ✨ feat(Auth): 카카오 액세스 토큰 발급 기능 추가

--- src/main/java/com/feelory/feelory_backend/global/security/auth/AuthController.java
+++ src/main/java/com/feelory/feelory_backend/global/security/auth/AuthController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -22,5 +23,13 @@ public class AuthController {
         KakaoLoginResponse kakaoLoginResponse = authService.createKakaoLoginUrl();
 
         return ApiResponse.success(kakaoLoginResponse, SuccessCode.CREATE_KAKAO_LOGIN_URL_SUCCESS);
+    }
+
+    @GetMapping("/kakao/callback")
+    public ApiResponse<KakaoAccessTokenResponse> kakaoCallback(@RequestParam("code") String code) {
+
+        KakaoAccessTokenResponse tokenResponse = authService.getKakaoAccessToken(code);
+
+        return ApiResponse.success(tokenResponse,SuccessCode.DEFAULT_SUCCESS);
     }
 }
--- src/main/java/com/feelory/feelory_backend/global/security/auth/AuthService.java
+++ src/main/java/com/feelory/feelory_backend/global/security/auth/AuthService.java
@@ -1,13 +1,17 @@
 package com.feelory.feelory_backend.global.security.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
     private final KakaoOauthProperties kakaoOauthProperties;
+    private final WebClient webClient = WebClient.create();
 
     public KakaoLoginResponse createKakaoLoginUrl() {
         String url = kakaoOauthProperties.getAuthUri() +
@@ -16,5 +20,18 @@ public class AuthService {
                 "&response_type=code";
 
         return new KakaoLoginResponse(url);
+    }
+
+    public KakaoAccessTokenResponse getKakaoAccessToken(String code) {
+        return webClient.post()
+                .uri("https://kauth.kakao.com/oauth/token")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .bodyValue("grant_type=authorization_code" +
+                        "&client_id=" + kakaoOauthProperties.getClientId()  +
+                        "&redirect_uri=" + kakaoOauthProperties.getRedirectUri() +
+                        "&code=" + code)
+                .retrieve()
+                .bodyToMono(KakaoAccessTokenResponse.class)
+                .block();
     }
 }
