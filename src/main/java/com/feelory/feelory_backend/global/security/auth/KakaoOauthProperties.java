c9741f59531fcafe74ab152eb0e6a2e0fa4e3dd0
DEV_KANGSAN <devstar1223@gmail.com>
2025-07-25 11:20:36 +0900

    🔧 chore(properties): 카카오 소셜 로그인 설정값 추가 및 바인딩

---
+++ src/main/java/com/feelory/feelory_backend/global/security/auth/KakaoOauthProperties.java
@@ -0,0 +1,17 @@
+package com.feelory.feelory_backend.global.security.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "kakao")
+@Getter
+@Setter
+public class KakaoOauthProperties {
+
+    private String clientId;
+    private String redirectUri;
+    private String authUri;
+}
