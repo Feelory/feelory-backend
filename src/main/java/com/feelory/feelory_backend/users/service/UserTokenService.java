cf05ba28eb684bb83ec9a903f8a5a574c495110f
DEV_KANGSAN <devstar1223@gmail.com>
2025-07-28 17:23:21 +0900

    ♻️ refactor(UserTokenService): 트랜잭션 어노테이션 제거 (상위 서비스에서 트랜잭션 관리 예정)

--- src/main/java/com/feelory/feelory_backend/users/service/UserTokenService.java
+++ src/main/java/com/feelory/feelory_backend/users/service/UserTokenService.java
@@ -5,7 +5,6 @@ import com.feelory.feelory_backend.users.entity.Users;
 import com.feelory.feelory_backend.users.repository.UserTokensRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -15,14 +14,13 @@ public class UserTokenService {
 
     private final UserTokensRepository userTokensRepository;
 
-    @Transactional
     public UserTokens saveUserToken(Users user, String refreshToken, LocalDateTime refreshTokenExp) {
         UserTokens token = UserTokens.builder()
                 .refreshToken(refreshToken)
                 .refreshTokenExp(refreshTokenExp)
                 .isActive(true)
                 .build();
-        user.addUserToken(token); // 유저 <-> 토큰 양방향 관계라면
+        user.addUserToken(token);
         return userTokensRepository.save(token);
     }
 }
