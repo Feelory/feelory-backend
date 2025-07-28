e5802acb2f62dffb724e9ef1ccda8d7093875563
DEV_KANGSAN <devstar1223@gmail.com>
2025-07-28 18:12:42 +0900

    🔧️ chore(SecurityConfig): 누락된 예외 처리 핸들러 추가

--- src/main/java/com/feelory/feelory_backend/global/config/SecurityConfig.java
+++ src/main/java/com/feelory/feelory_backend/global/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.feelory.feelory_backend.global.config;
 
+import com.feelory.feelory_backend.global.security.auth.handler.CustomAccessDeniedHandler;
+import com.feelory.feelory_backend.global.security.auth.handler.CustomAuthenticationEntryPoint;
 import com.feelory.feelory_backend.global.security.auth.handler.CustomOAuth2AuthenticationSuccessHandler;
 import com.feelory.feelory_backend.global.security.auth.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
@@ -16,12 +18,18 @@ public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomOAuth2AuthenticationSuccessHandler authenticationSuccessHandler;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+                        .accessDeniedHandler(customAccessDeniedHandler)
+                )
                 .authorizeHttpRequests(authz -> authz
 //                        .requestMatchers("/public-example-api").permitAll()
 //                        .anyRequest().authenticated()
