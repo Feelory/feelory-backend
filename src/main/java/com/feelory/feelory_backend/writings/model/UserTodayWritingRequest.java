18483f94deebd1836906142cc67fc520883d248b
tryoo0607 <tryoo0607@gmail.com>
2025-07-30 10:18:03 +0900

    🔧 fix(DailyWordWritings) : 내가 쓴 글 조회 API의 request 어노테이션을 Builder -> Setter로 교체

--- src/main/java/com/feelory/feelory_backend/writings/model/UserTodayWritingRequest.java
+++ src/main/java/com/feelory/feelory_backend/writings/model/UserTodayWritingRequest.java
@@ -1,16 +1,13 @@
 package com.feelory.feelory_backend.writings.model;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 /*
     TODO. [TR-YOO] 로그인 기능 완성되면 추후 해당 객체 삭제
 */
 @Getter
-@Builder
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserTodayWritingRequest {
