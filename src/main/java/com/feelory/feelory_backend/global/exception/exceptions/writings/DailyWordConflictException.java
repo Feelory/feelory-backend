0ff988b466e3afafd2523bc77de2a37a15855940
tryoo0607 <tryoo0607@gmail.com>
2025-08-13 21:32:08 +0900

    🐛 fix(Exception): 잘못된 Exception Message 수정

--- src/main/java/com/feelory/feelory_backend/global/exception/exceptions/writings/DailyWordConflictException.java
+++ src/main/java/com/feelory/feelory_backend/global/exception/exceptions/writings/DailyWordConflictException.java
@@ -5,6 +5,6 @@ import com.feelory.feelory_backend.global.exception.exceptions.ErrorCode;
 
 public class DailyWordConflictException extends BaseException {
     public DailyWordConflictException() {
-        super(ErrorCode.WRITING_GOAL_CONFLICT);
+        super(ErrorCode.DAILY_WORD_CONFLICT);
     }
 }
