2e234080c0fac804417a7fea54ff6e0fd10bd691
DEV_KANGSAN <devstar1223@gmail.com>
2025-08-20 14:35:15 +0900

    ♻️ refactor(Security): JWT의 유저 id 추출 후 검사 기능 필터로 이동, if문 중첩 대신 early return 방식 적용

--- src/main/java/com/feelory/feelory_backend/global/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/feelory/feelory_backend/global/security/jwt/JwtAuthenticationFilter.java
@@ -1,12 +1,13 @@
 package com.feelory.feelory_backend.global.security.jwt;
 
 import com.feelory.feelory_backend.domain.user.service.UserService;
-import com.feelory.feelory_backend.global.exception.exceptions.auth.UserAccessDeniedException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,7 +16,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.nio.file.AccessDeniedException;
 import java.util.Collections;
 
 @Component
@@ -28,30 +28,36 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+
         String header = request.getHeader("Authorization");
 
-        if (header != null && header.startsWith("Bearer ")) {
-            String token = header.substring(7);
-
-            if (jwtProvider.validateToken(token)) {
-                Long userId = jwtProvider.getUserIdFromToken(token);
-
-                if (!userService.isActiveUser(userId)) {
-                    throw new UserAccessDeniedException();
-                }
-
-                String role = jwtProvider.getRoleFromToken(token);
-
-                UsernamePasswordAuthenticationToken authentication =
-                        new UsernamePasswordAuthenticationToken(
-                                userId, null,
-                                Collections.singletonList(new SimpleGrantedAuthority("ROLE_"+role))
-                        );
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
         }
+
+        String token = header.substring(7);
+        if (!jwtProvider.validateToken(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        Long userId = jwtProvider.getUserIdFromToken(token);
+        if (!userService.isActiveUser(userId)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String role = jwtProvider.getRoleFromToken(token);
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(
+                        userId,
+                        null,
+                        Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role))
+                );
+
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
         filterChain.doFilter(request, response);
     }
