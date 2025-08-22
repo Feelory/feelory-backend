9a06da476c895c3945fd69628731d6a5235ec808
tryoo0607 <tryoo0607@gmail.com>
2025-08-22 19:17:50 +0900

    ✨ feat(GeminiFeedbackProperty): JSON 응답 구조 설정에 최솟값, 최댓값 옵션 추가

--- src/main/java/com/feelory/feelory_backend/global/webclient/annotation/GeminiFeedbackProperty.java
+++ src/main/java/com/feelory/feelory_backend/global/webclient/annotation/GeminiFeedbackProperty.java
@@ -5,6 +5,7 @@ import java.lang.annotation.*;
 
 /*
     Gemini responseSchema/응답 JSON 생성을 위한 필드 메타데이터.
+    참고 : https://ai.google.dev/api/caching?hl=ko#Schema
 */
 
 @Retention(RetentionPolicy.RUNTIME)         // 리플렉션 통해서 메타데이터 추출하기 위해 사용
@@ -16,4 +17,8 @@ public @interface GeminiFeedbackProperty {
     boolean required() default true;        // 필수 필드 여부
     String[] enumValues() default {};       // 사용할 ENUM 목록 (주로 GeminiPropertyType.STRING 일 때 사용 가능)
     String name() default "";               // JSON 키(미지정 시 필드명 사용)
+    int minLength();                        // GeminiPropertyType.STRING 일 때 최소 길이
+    int maxLength();                        // GeminiPropertyType.STRING 일 때 최대 길이
+    int minimum();                          // GeminiPropertyType.INTEGER 일 때 최솟값
+    int maximum();                          // GeminiPropertyType.INTEGER 일 때 최댓값
 }
